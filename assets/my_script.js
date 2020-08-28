1a37573719da5ed44f69f7686e324ae5e1d987ac
Akshay G J <31623511+AkshayGJ@users.noreply.github.com>
2020-08-28 15:05:45 +0530

    kssearch

---
+++ assets/my_script.js
@@ -0,0 +1,16 @@
+$('.searchbar-group').click(function() {
+	$('.filter-list').hide();
+	var selected = [];    
+	var fill = [];
+	$('.searchbar-checkbox:checked').each(function() {
+    selected.push($(this));
+  });
+  if(selected.length==0)
+  	$('.filter-list').show();
+  var i=0;
+  for(i=0;i<selected.length;i++) {
+    $('.'+selected[i].val()).each(function() {
+    	$(this).show();
+    });
+  }
+})
