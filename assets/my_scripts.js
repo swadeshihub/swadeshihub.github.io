4177e2b07617d254226c7471afaea03bfddbbccd
Akshay G J <31623511+AkshayGJ@users.noreply.github.com>
2020-08-29 09:59:08 +0530

    000

--- assets/my_scripts.js
+++ assets/my_scripts.js
@@ -2,6 +2,7 @@ function filter() {
 	$('.filter-list').hide();
 	var selected = [];
 	$('input[name="chek-filter"]:checked').each(function() {
+    $('.'+$(this).val()).show();
     alert($(this).val());
     selected.push($(this));
     $('.'+$(this).val()).each(function() {
